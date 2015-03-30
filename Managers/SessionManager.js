d6d3b3af77cf37412ec5ce08853c6b50c9adb391
Callum Fraser <email@callumjfraser.com>
2015-03-30 21:47:14 +0100

    Adding the session manager class.
    Adding the session manager class.

---
+++ Managers/SessionManager.js
@@ -0,0 +1,44 @@
+var async = require('async');
+var databaseObject = require('../Database');
+var Failed = require('../Failed');
+
+module.exports = {
+	Get: function (id, callback){
+		GetByID(id, callback);
+	}
+};
+
+function Class(row, callback){
+	if(row == undefined)
+		return new Failed('Missing parameter');
+	var object = {};
+	object.ID = row.ID;
+	object.Start = row.Start;
+	object.End = row.End;
+	object.RunthroughID = row.RunthroughID;
+	callback(object);
+}
+
+function GetByID(id, callback){
+	if(id == undefined){
+		callback(new Failed('Missing parameter'));
+	}
+	else{
+		var intID = parseInt(id);
+		if(intID > 0){
+			databaseObject.Procedure('sp_GetSessionByID', [id], function(rows){
+				if(rows.length > 0){
+					Class(rows[0], function(value){
+						callback(value);
+					});
+				}
+				else{
+					callback(new Failed('No matching results'));
+				}
+			});
+		}
+		else{
+			callback(new Failed('Invalid parameter'));
+		}
+	}
+}
