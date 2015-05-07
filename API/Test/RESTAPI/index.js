ef868c93c0e5cf54763a78abeaa522ebdd360d97
Callum Fraser <email@callumjfraser.com>
2015-05-07 22:19:21 +0100

    Adding the test for the invalid password API call. It is missing a check on the HTTP Status.
    Adding the test for the invalid password API call. It is missing a
    check on the HTTP Status.

--- API/Test/RESTAPI/index.js
+++ API/Test/RESTAPI/index.js
@@ -4,17 +4,6 @@ var http = require("http");
 var assert = require('assert');
 var RESTAPI = require('../../index.js');
 
-var options = {
-	hostname: 'localhost',
-	port: 1024,
-	path: '/login/Test1',
-	method: 'GET',
-	headers: {
-		'Password': 'password',
-		'Content-Length': 0
-	}
-};
-
 module.exports = {
 	Test: function(){
 
@@ -28,6 +17,17 @@ module.exports = {
 
 		describe('Login:', function(){
 			it('Valid:', function(done){
+				var options = {
+					hostname: 'localhost',
+					port: 1024,
+					path: '/login/Test1',
+					method: 'GET',
+					headers: {
+						'Password': 'password',
+						'Content-Length': 0
+					}
+				};
+
 				http.request(options,
 					function(res) {
 						res.setEncoding('utf8');
@@ -38,6 +38,36 @@ module.exports = {
 							assert.notEqual(result.Username, undefined);
 							assert.notEqual(result.AID, undefined);
 							assert.notEqual(result.SID, undefined);
+							done();
+						});
+					}
+				).end();
+			});
+
+			it('Invalid Password:', function(done){
+				var options = {
+					hostname: 'localhost',
+					port: 1024,
+					path: '/login/Test1',
+					method: 'GET',
+					headers: {
+						'Password': 'notthepassword',
+						'Content-Length': 0
+					}
+				};
+
+				http.request(options,
+					function(res) {
+						res.setEncoding('utf8');
+//						console.log(res.statusCode);
+						res.on('data', function (chunk) {
+							var result = JSON.parse(chunk);
+							assert.equal(result.Success, false);
+							assert.notEqual(result.Reason, undefined)
+							assert.equal(result.AccountID, undefined);
+							assert.equal(result.Username, undefined);
+							assert.equal(result.AID, undefined);
+							assert.equal(result.SID, undefined);
 							done();
 						});
 					}
