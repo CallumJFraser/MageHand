49e9854450491fac20e713d2e6a562f4c101a3ee
Callum Fraser <email@callumjfraser.com>
2015-05-22 20:07:00 +0100

    Updating the Runthrough Manager tests in line with the current test changes.
    Updating the Runthrough Manager tests in line with the current test
    changes.

--- API/Test/Managers/RunthroughManager.js
+++ API/Test/Managers/RunthroughManager.js
@@ -3,56 +3,59 @@
 var assert = require('assert');
 var manager = require('../../Managers/RunthroughManager.js');
 
-var blank = undefined;
+describe('Manager - Runthrough:', function(){
 
-describe('Public functions:', function(){
-	it('Get != undefined', function(){
-		assert.notEqual(manager.Get, undefined);
-	})
-})
+	var blank = undefined;
 
-describe('Get:', function(){
-	var valid = 1;
-	var invalid = 0;
-	var invalidFormat = 'invalid';
-
-	it('Valid:', function(done){
-		manager.Get(valid, function(result){
-			assert.notEqual(result, undefined);
-			assert.equal(result.Reason, undefined);
-			assert.notEqual(result.ID, undefined);
-			assert.notEqual(result.Story, undefined);
-			done();
-		});
+	describe('Public functions:', function(){
+		it('Get != undefined', function(){
+			assert.notEqual(manager.Get, undefined);
+		})
 	})
 
-	it('Invalid "ID" Value:', function(done){
-		manager.Get(invalid, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
-	})
+	describe('Get:', function(){
+		var valid = 1;
+		var invalid = 0;
+		var invalidFormat = 'invalid';
 
-	it('Invalid "ID" Format:', function(done){
-		manager.Get(invalidFormat, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);	
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
-	})
+		it('Valid:', function(done){
+			manager.Get(valid, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.equal(result.Reason, undefined);
+				assert.notEqual(result.ID, undefined);
+				assert.notEqual(result.Story, undefined);
+				done();
+			});
+		})
+	
+		it('Invalid "ID" Value:', function(done){
+			manager.Get(invalid, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
 
-	it('Missing "ID":', function(done){
-		manager.Get(blank, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
+		it('Invalid "ID" Format:', function(done){
+			manager.Get(invalidFormat, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);	
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
+
+		it('Missing "ID":', function(done){
+			manager.Get(blank, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
 	})
 })
