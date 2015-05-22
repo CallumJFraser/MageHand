8a08552892df1651d7a687341a5964ce9ffe5ac4
Callum Fraser <email@callumjfraser.com>
2015-05-22 20:03:03 +0100

    Updating the class manager to resolve the test changes.
    Updating the class manager to resolve the test changes.

--- API/Managers/ClassManager.js
+++ API/Managers/ClassManager.js
@@ -14,68 +14,70 @@ function Class(row, callback){
 	async.map(
 		[row.VersionID],
 		function(item, parallelCallback){
-			versionManager.Get(item, function(result){
-				parallelCallback(null, result)
+			versionManager.Get(item, function(err, result){
+				parallelCallback(err, result)
 			});
 		},
 		function(err, results){
 			if(row == undefined)
-				return new Failed('Missing parameter');
+				callback('Row undefined', Failed('Missing parameter'));
+			if(err)
+				callback(err, Failed('There was an error.'));
 			var object = {};
 			object.ID = row.ID;
 			object.Name = row.Name;
 			object.Description = row.Description;
 			object.Version = results[0];
-			callback(object);
+			callback(undefined, object);
 		}
 	);
 }
 
 function GetByID(id, callback){
 	if(id == undefined){
-		callback(new Failed('Missing parameter'));
+		callback('Id undefined', new Failed('Missing parameter'));
 	}
 	else{
 		var intID = parseInt(id);
 		if(intID > 0){
-			databaseObject.Procedure('sp_GetClass', [id], function(rows){
-				if(rows.length > 0){
-					Class(rows[0], function(value){
-						callback(value);
+			databaseObject.Procedure('sp_GetClass', [id], function(err, rows){
+				if(rows.length > 0 && !err){
+					Class(rows[0], function(err, value){
+						callback(err, value);
 					});
 				}
 				else{
-					callback(new Failed('No matching results'));
+					callback(err, new Failed('No matching results'));
 				}
 			});
 		}
 		else{
-			callback(new Failed('Invalid parameter'));
+			callback(intID, new Failed('Invalid parameter'));
 		}
 	}
 }
 
 function GetList(callback){
-	databaseObject.Procedure('sp_GetClasses', [], function(rows){
-		if(rows.length > 0){
+	databaseObject.Procedure('sp_GetClasses', [], function(err, rows){
+		if(rows.length > 0 && !err){
 			var characterLists = [];
 			async.map(rows, function(item, eachCallback){
-					Class(item, function(value){
-						eachCallback(null, value);
+					Class(item, function(err, value){
+						eachCallback(err, value);
 					});
 				},
 				function(err, results){
 					if(err != undefined){
-						console.log('Error');
+						callback(err, undefined);
 					}
 					else{
-						callback(results);
+						callback(undefined, results);
 					}
 				}
 			);	
 		}
 		else{
-			callback(new Failed('No matching results'));
+			callback('No results', new Failed('No matching results'));
 		}
 	});
 }
