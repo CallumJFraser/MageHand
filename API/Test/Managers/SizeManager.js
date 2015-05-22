5119130875822cd0611744cb0872c722c86c488d
Callum Fraser <email@callumjfraser.com>
2015-05-22 20:08:22 +0100

    Updating the Size Manager tests in line with the current test changes.
    Updating the Size Manager tests in line with the current test changes.

--- API/Test/Managers/SizeManager.js
+++ API/Test/Managers/SizeManager.js
@@ -3,57 +3,60 @@
 var assert = require('assert');
 var manager = require('../../Managers/SizeManager.js');
 
-var blank = undefined;
+describe('Manager - Size:', function(){
+	
+	var blank = undefined;
 
-describe('Public functions:', function(){
-	it('Get != undefined', function(){
-		assert.notEqual(manager.Get, undefined);
-	})
-})
-
-describe('Get:', function(){
-	var valid = 1;
-	var invalid = 0;
-	var invalidFormat = 'invalid';
-
-	it('Valid:', function(done){
-		manager.Get(valid, function(result){
-			assert.notEqual(result, undefined);
-			assert.equal(result.Reason, undefined);
-			assert.notEqual(result.ID, undefined);
-			assert.notEqual(result.Name, undefined);
-			done();
-		});
+	describe('Public functions:', function(){
+		it('Get != undefined', function(){
+			assert.notEqual(manager.Get, undefined);
+		})
 	})
 
-	it('Invalid "ID" Value:', function(done){
-		manager.Get(invalid, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Name, undefined);
-			done();
-		});
-	})
+	describe('Get:', function(){
+		var valid = 1;
+		var invalid = 0;
+		var invalidFormat = 'invalid';
 
-	it('Invalid "ID" Format:', function(done){
-		manager.Get(invalidFormat, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);	
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Name, undefined);
-			done();
-		});
-	})
+		it('Valid:', function(done){
+			manager.Get(valid, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.equal(result.Reason, undefined);
+				assert.notEqual(result.ID, undefined);
+				assert.notEqual(result.Name, undefined);
+				done();
+			});
+		})
 
-	it('Missing "ID":', function(done){
-		manager.Get(blank, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.Success, false);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Name, undefined);
-			done();
-		});
+		it('Invalid "ID" Value:', function(done){
+			manager.Get(invalid, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Name, undefined);
+				done();
+			});
+		})
+
+		it('Invalid "ID" Format:', function(done){
+			manager.Get(invalidFormat, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);	
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Name, undefined);
+				done();
+			});
+		})
+
+		it('Missing "ID":', function(done){
+			manager.Get(blank, function(err, result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.Success, false);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Name, undefined);
+				done();
+			});
+		})
 	})
 })
