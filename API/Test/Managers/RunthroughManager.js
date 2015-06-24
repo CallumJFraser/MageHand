2fbc5a75e614a9489710045313bfffbed7ba6dfc
callum fraser <email@callumjfraser.com>
2015-06-24 21:55:11 +0100

    Updating the runthroughManager tests to mock the database calls.

--- API/Test/Managers/RunthroughManager.js
+++ API/Test/Managers/RunthroughManager.js
@@ -1,58 +1,104 @@
 "Use Strict";
 
 var assert = require('assert');
+var proxyquire = require('proxyquire').noCallThru();
 var manager = require('../../Managers/RunthroughManager.js');
 
 var blank = undefined;
 
-describe('Public functions:', function(){
-	it('Get != undefined', function(){
-		assert.notEqual(manager.Get, undefined);
-	})
-})
+describe('Runthrough Manager', function () {
+	var fakeDatabase = {};
+	var fakeManager = proxyquire('../../Managers/RunthroughManager.js', {
+		'../Database': fakeDatabase,
+		'../Managers/StoryManager': {
+			Get: function (item, callback) {
+				callback({});
+			}
+		}
+	});
 
-describe('Get:', function(){
-	var valid = 1;
-	var invalid = 0;
-	var invalidFormat = 'invalid';
-
-	it('Valid:', function(done){
-		manager.Get(valid, function(result){
-			assert.notEqual(result, undefined);
-			assert.equal(result.Reason, undefined);
-			assert.notEqual(result.ID, undefined);
-			assert.notEqual(result.Story, undefined);
-			done();
-		});
+	describe('Public functions:', function(){
+		it('Get != undefined', function(){
+			assert.notEqual(manager.Get, undefined);
+		})
 	})
 
-	it('Invalid "ID" Value:', function(done){
-		manager.Get(invalid, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
-	})
+	describe('Get:', function(){
+		var valid = 1;
+		var invalid = 0;
+		var invalidFormat = 'invalid';
 
-	it('Invalid "ID" Format:', function(done){
-		manager.Get(invalidFormat, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);	
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
-	})
+		it('Valid:', function(done){
+			var validRow = {"ID":1,"Name":"Test","Description":"Test","Speed":10,"Size":1,"Version":1};
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetRunthroughByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], valid);
+				assert.notEqual(callback, undefined);
+				callback([validRow]);
+			};
 
-	it('Missing "ID":', function(done){
-		manager.Get(blank, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Story, undefined);
-			done();
-		});
-	})
-})
+			fakeManager.Get(valid, function(result){
+				assert.notEqual(result, undefined);
+				assert.equal(result.Reason, undefined);
+				assert.notEqual(result.ID, undefined);
+				assert.notEqual(result.Story, undefined);
+				done();
+			});
+		})
+
+		it('Invalid "ID" Value:', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetRunthroughByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], invalid);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(invalid, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
+
+		it('Invalid "ID" Format:', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetRunthroughByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], invalidFormat);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(invalidFormat, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);	
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
+
+		it('Missing "ID":', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetRunthroughByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], blank);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(blank, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Story, undefined);
+				done();
+			});
+		})
+	});
+});
