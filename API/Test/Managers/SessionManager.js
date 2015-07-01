afc2bfb2d11d8297d12c4b5b859f34e0c1117c93
callum fraser <email@callumjfraser.com>
2015-07-01 23:53:59 +0100

    ;Adding database mocking to the SessionManager tests.

--- API/Test/Managers/SessionManager.js
+++ API/Test/Managers/SessionManager.js
@@ -1,67 +1,107 @@
 "Use Strict";
 
 var assert = require('assert');
+var proxyquire = require('proxyquire').noCallThru();
 var manager = require('../../Managers/SessionManager.js');
 
 var blank = undefined;
 
-describe('Public functions:', function(){
-	it('Get != undefined', function(){
-		assert.notEqual(manager.Get, undefined);
-	})
-})
+describe.only('Session Manager', function() {
+	var fakeDatabase = {};
+	var fakeManager = proxyquire('../../Managers/SessionManager.js', {
+		'../Database': fakeDatabase
+	});
 
-describe('Get:', function(){
-	var valid = 1;
-	var invalid = 0;
-	var invalidFormat = 'invalid';
-
-	it('Valid:', function(done){
-		manager.Get(valid, function(result){
-			assert.notEqual(result, undefined);
-			assert.equal(result.Reason, undefined);
-			assert.notEqual(result.ID, undefined);
-			assert.notEqual(result.Start, undefined);
-			//	Optional param set to null in db at the moment.
-//					assert.notEqual(result.End, undefined);
-			assert.notEqual(result.RunthroughID, undefined);
-			done();
-		});
+	describe('Public functions:', function(){
+		it('Get != undefined', function(){
+			assert.notEqual(manager.Get, undefined);
+		})
 	})
 
-	it('Invalid "ID" Value:', function(done){
-		manager.Get(invalid, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Start, undefined);
-			assert.equal(result.End, undefined);
-			assert.equal(result.RunthroughID, undefined);
-			done();
-		});
-	})
+	describe('Get:', function(){
+		var valid = 1;
+		var invalid = 0;
+		var invalidFormat = 'invalid';
 
-	it('Invalid "ID" Format:', function(done){
-		manager.Get(invalidFormat, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Start, undefined);
-			assert.equal(result.End, undefined);
-			assert.equal(result.RunthroughID, undefined);
-			done();
-		});
-	})
+		it('Valid:', function(done){
+			var validRow = {"ID":1,"Start":"0101010101","End":"0101010103","RunthroughID":1};
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetSessionByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], valid);
+				assert.notEqual(callback, undefined);
+				callback([validRow]);
+			};
 
-	it('Missing "ID":', function(done){
-		manager.Get(blank, function(result){
-			assert.notEqual(result, undefined);
-			assert.notEqual(result.Reason, undefined);
-			assert.equal(result.ID, undefined);
-			assert.equal(result.Start, undefined);
-			assert.equal(result.End, undefined);
-			assert.equal(result.RunthroughID, undefined);
-			done();
-		});
+			fakeManager.Get(valid, function(result){
+				assert.notEqual(result, undefined);
+				assert.equal(result.Reason, undefined);
+				assert.notEqual(result.ID, undefined);
+				assert.notEqual(result.Start, undefined);
+				assert.notEqual(result.End, undefined);
+				assert.notEqual(result.RunthroughID, undefined);
+				done();
+			});
+		})
+
+		it('Invalid "ID" Value:', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetSessionByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], invalid);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(invalid, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Start, undefined);
+				assert.equal(result.End, undefined);
+				assert.equal(result.RunthroughID, undefined);
+				done();
+			});
+		})
+
+		it('Invalid "ID" Format:', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetSessionByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], invalidFormat);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(invalidFormat, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Start, undefined);
+				assert.equal(result.End, undefined);
+				assert.equal(result.RunthroughID, undefined);
+				done();
+			});
+		})
+
+		it('Missing "ID":', function(done){
+			fakeDatabase.Procedure = function (procedure, values, callback) {
+				assert.equal(procedure, 'sp_GetSessionByID');
+				assert.equal(values.length, 1);
+				assert.equal(values[0], blank);
+				assert.notEqual(callback, undefined);
+				callback([]);
+			};
+
+			fakeManager.Get(blank, function(result){
+				assert.notEqual(result, undefined);
+				assert.notEqual(result.Reason, undefined);
+				assert.equal(result.ID, undefined);
+				assert.equal(result.Start, undefined);
+				assert.equal(result.End, undefined);
+				assert.equal(result.RunthroughID, undefined);
+				done();
+			});
+		})
 	})
 })
